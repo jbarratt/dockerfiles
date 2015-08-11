08c29ef8b6dfb21fda568b8737d024089b6fa742
Joshua Barratt <jbarratt@serialized.net>
2015-08-11 10:58:19 -0700

    bugfixes with configurable passwords

--- mutt/entrypoint.sh
+++ mutt/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-sed -i "s/%OFFICE_USERNAME%/$OFFICE_USERNAME/g" "$HOME/.mutt/muttrc"
-sed -i "s/%OFFICE_PASSWORD%/$OFFICE_PASSWORD/g" "$HOME/.mutt/muttrc"
-sed -i "s/%OFFICE_REALNAME%/$OFFICE_REALNAME/g" "$HOME/.mutt/muttrc"
-sed -i "s/%OFFICE_EMAIL%/$OFFICE_EMAIL/g" "$HOME/.mutt/muttrc"
+sed -i "s/OFFICE_USERNAME/$OFFICE_USERNAME/g" "$HOME/.mutt/muttrc"
+sed -i "s/OFFICE_PASSWORD/$OFFICE_PASSWORD/g" "$HOME/.mutt/muttrc"
+sed -i "s/OFFICE_REALNAME/$OFFICE_REALNAME/g" "$HOME/.mutt/muttrc"
+sed -i "s/OFFICE_EMAIL/$OFFICE_EMAIL/g" "$HOME/.mutt/muttrc"
 
 
 if [ -e "$HOME/.muttrc.local" ]; then
--- run-mutt.sh
+++ run-mutt.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-OFFICE_USERNAME=$(security find-generic-password -s jomax | grep acct | cut -d '"' -f 4)
-OFFICE_PASSWORD=$(security find-generic-password -s jomax -w)
-OFFICE_REALNAME="Joshua Barratt"
-OFFICE_EMAIL="josh@mediatemple.net"
+export OFFICE_USERNAME=$(security find-generic-password -s jomax | grep acct | cut -d '"' -f 4)
+export OFFICE_PASSWORD=$(security find-generic-password -s jomax -w)
+export OFFICE_REALNAME="Joshua Barratt"
+export OFFICE_EMAIL="josh@mediatemple.net"
 
 #docker rm $(docker ps -afq name=mutt)
+
 docker run --rm -it \
     -v $HOME/.vim:/home/user/.vim \
     -e OFFICE_USERNAME -e OFFICE_PASSWORD -e OFFICE_REALNAME -e OFFICE_EMAIL \
